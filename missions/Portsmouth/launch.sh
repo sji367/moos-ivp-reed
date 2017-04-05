bcb775f5beb5e4de6d79e088fb3bb51cf43ae3c6
Sam Reed <sji367@wildcats.unh.edu>
2017-04-05 01:00:27 -0400

    new mission

---
+++ missions/Portsmouth/launch.sh
@@ -0,0 +1,70 @@
+#!/bin/bash 
+COMMUNITY="alpha"
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+TIME_WARP=1
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+	printf "%s [SWITCHES] [time_warp]   \n" $0
+	printf "  --help, -h         \n" 
+	exit 0;
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+        TIME_WARP=$ARGI
+    else 
+	printf "Bad Argument: %s \n" $ARGI
+	exit 0
+    fi
+done
+
+#-------------------------------------------------------
+#  Part 2: Create the .moos and .bhv files. 
+#-------------------------------------------------------
+#nsplug $COMMUNITY.moos OA.moos
+
+
+#-------------------------------------------------------
+#  Part 3: Launch the processes
+#-------------------------------------------------------
+printf "Launching the %s MOOS Community (WARP=%s) \n"  $COMMUNITY $TIME_WARP
+pAntler alpha.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+
+sleep 0.2
+printf "Launching Config Reader \n"
+python ../../src/Python/config_reader.py >& /dev/null &
+
+sleep 1
+printf "Launching MOOSTides \n"
+python ../../src/Python/MOOSTides.py >& /dev/null &
+
+sleep 1
+printf "Launching pub_points \n"
+#ENC_Contact \n
+#python ../../src/Python/ENC_Contact.py >& /dev/null &
+#python ../../src/Python/pub_points.py >& /dev/null &
+
+sleep 1
+printf "Launching ENC_Print \n"
+#python ../../src/Python/ENC_Print.py >& /dev/null &
+
+sleep 0.2
+printf "Launching ENC_WPT_check \n"
+python ../../src/Python/ENC_WPT_check.py >& /dev/null &
+
+sleep 0.25
+#printf "Launching ENC_SFoV \n"
+#python ../../src/Python/ENC_SFoV.py >& /dev/null &
+
+sleep 0.2
+#printf "Launching ENC_SFoV_GUI \n"
+#python ../../src/Python/ENC_Sensor_GUI.py >& /dev/null &
+
+uMAC alpha.moos
+
+
+printf "Killing all processes ... \n"
+kill %1 %2 %3 %4 #%5 #%6 %7 #%8
+#mykill
+printf "Done killing processes.   \n"
+
---
+++ missions/Portsmouth/launch_zboat.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+COMMUNITY="zboat"
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+TIME_WARP=1
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+	printf "%s [SWITCHES] [time_warp]   \n" $0
+	printf "  --help, -h         \n" 
+	exit 0;
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+        TIME_WARP=$ARGI
+    else 
+	printf "Bad Argument: %s \n" $ARGI
+	exit 0
+    fi
+done
+
+
+#-------------------------------------------------------
+#  Part 2: Launch the processes
+#-------------------------------------------------------
+printf "Launching the %s MOOS Community (WARP=%s) \n"  $COMMUNITY $TIME_WARP
+pAntler $COMMUNITY.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+
+uMAC $COMMUNITY.moos
+
+sleep 0.2
+printf "Launching Config Reader \n"
+python ../../src/Python/config_reader.py >& /dev/null &
+
+sleep 1
+printf "Launching pub_points \n"
+python ../../src/Python/pub_points.py >& /dev/null &
+
+
+
+printf "Killing all processes ... \n"
+kill %1 %2 %3
+mykill
+./../../scripts/stop_zboat.py
+printf "Done killing processes.   \n"
